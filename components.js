acd485bc29c97152ad2649e6c17f3105397b7e6c
Julie VanderLaan julieannevl@gmail.com <julieannevl@gmail.com>
2018-11-29 18:52:18 +0100

    removed some js

--- components.js
+++ components.js
@@ -1,6 +1,6 @@
 //SVG fallback
 
-function svgasimg() {
-  return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
-}
+// function svgasimg() {
+//   return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
+// }
 
